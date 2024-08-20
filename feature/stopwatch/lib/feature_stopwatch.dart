63199d963f5f180435152f1cc0fa07b3de031fec
Ryo Takeuchi <develop@tatsutakein.jp>
2024-08-20 20:31:17 +0900

    feat(stopwatch): stopwatch パッケージを追加

---
+++ feature/stopwatch/lib/feature_stopwatch.dart
@@ -0,0 +1,5 @@
+import 'package:feature_stopwatch/src/gen/l10n/l10n.dart';
+
+export 'src/ui/screen/stopwatch/stopwatch_screen.dart';
+
+typedef FeatureStopwatchL10n = L10n;
---
+++ feature/stopwatch/lib/src/ui/screen/stopwatch/component/notification_section.dart
@@ -0,0 +1,43 @@
+import 'package:core_designsystem/component.dart';
+import 'package:feature_stopwatch/src/gen/l10n/l10n.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final class NotificationSection extends HookConsumerWidget {
+  const NotificationSection({
+    required void Function() onMoreButtonPressed,
+    super.key,
+  }) : _onMoreButtonPressed = onMoreButtonPressed;
+
+  final VoidCallback _onMoreButtonPressed;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final l10n = L10n.of(context);
+
+    return Container(
+      width: double.infinity,
+      child: Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            child: Text(
+              l10n.notification,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const Gap(16),
+          FilledButton(
+            onPressed: _onMoreButtonPressed,
+            child: Text(
+              l10n.notification,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
