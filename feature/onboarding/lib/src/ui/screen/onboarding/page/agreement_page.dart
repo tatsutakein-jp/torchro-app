676cd2eca8abc03c425b6e7af1e1e07366a37e62
Ryo Takeuchi <develop@tatsutakein.jp>
2024-08-21 19:20:17 +0900

    feat(onboarding): lottie アニメーションを追加

--- feature/onboarding/lib/src/ui/screen/onboarding/page/agreement_page.dart
+++ feature/onboarding/lib/src/ui/screen/onboarding/page/agreement_page.dart
@@ -2,9 +2,11 @@ import 'package:core_common/anyhow.dart';
 import 'package:core_designsystem/component.dart';
 import 'package:core_designsystem/space.dart';
 import 'package:core_domain/user_settings.dart';
+import 'package:feature_onboarding/src/gen/assets/assets.gen.dart';
 import 'package:feature_onboarding/src/gen/l10n/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:lottie/lottie.dart';
 
 /// 同意して始めるページ
 final class AgreementPage extends ConsumerWidget {
@@ -19,37 +21,48 @@ final class AgreementPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final l10n = L10n.of(context);
 
-    return Padding(
-      padding: const EdgeInsets.all(TorchroSpace.l),
-      child: Column(
-        children: [
-          SizedBox(
-            width: double.infinity,
-            child: Text(
-              l10n.agreeAndStart,
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(TorchroSpace.l),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ...[
+              Text(
+                l10n.agreeAndStart,
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
+              Lottie.asset(
+                Assets.lottie.torch,
+                width: 250,
+                height: 250,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: FilledButton(
+                  onPressed: () async {
+                    switch (await ref.read(agreeUseCaseProvider)) {
+                      case Ok():
+                        _onAgreeAndContinue();
+                      case Err():
+                        // TODO: エラー処理
+                        break;
+                    }
+                  },
+                  child: Text(l10n.agreeAndStart),
+                ),
+              ),
+            ].expand(
+              (widget) => [
+                widget,
+                const Gap(TorchroSpace.m),
+              ],
             ),
-          ),
-          const Gap(TorchroSpace.m),
-          SizedBox(
-            width: double.infinity,
-            child: FilledButton(
-              onPressed: () async {
-                switch (await ref.read(agreeUseCaseProvider)) {
-                  case Ok():
-                    _onAgreeAndContinue();
-                  case Err():
-                    // TODO: エラー処理
-                    break;
-                }
-              },
-              child: Text(l10n.agreeAndStart),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
