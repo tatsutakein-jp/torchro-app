1a74f0471ba7ce4ea95db22b1f0ddf3d277cb394
Ryo Takeuchi <develop@tatsutakein.jp>
2024-10-26 08:21:38 +0900

    fix: generated files

--- core/domain/lib/src/use_case/contact/contact_by_id_stream_use_case.g.dart
+++ core/domain/lib/src/use_case/contact/contact_by_id_stream_use_case.g.dart
@@ -156,6 +156,8 @@ class ContactByIdStreamUseCaseProvider
   }
 }
 
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
 mixin ContactByIdStreamUseCaseRef on AutoDisposeStreamProviderRef<Contact?> {
   /// The parameter `id` of this provider.
   ContactId get id;
@@ -170,4 +172,4 @@ class _ContactByIdStreamUseCaseProviderElement
   ContactId get id => (origin as ContactByIdStreamUseCaseProvider).id;
 }
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
