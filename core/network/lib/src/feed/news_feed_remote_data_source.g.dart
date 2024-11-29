53cdc21f099886c1a7f968e274a9f905cc583883
Ryo Takeuchi <develop@tatsutakein.jp>
2024-11-29 22:45:35 +0900

    fix(gen): melos gen

--- core/network/lib/src/feed/news_feed_remote_data_source.g.dart
+++ core/network/lib/src/feed/news_feed_remote_data_source.g.dart
@@ -6,7 +6,7 @@ part of 'news_feed_remote_data_source.dart';
 // RetrofitGenerator
 // **************************************************************************
 
-// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations
 
 class _NewsFeedRemoteDataSource implements NewsFeedRemoteDataSource {
   _NewsFeedRemoteDataSource(
